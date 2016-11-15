fea3de390ad6e4aff0bad0ce7441624fb89a3cd6
Gijoe614 <jvandoorn@students.mchenry.edu>
2016-11-15 13:16:06 -0600

    Add files via upload

---
+++ Exercise-17.3.py
@@ -0,0 +1,15 @@
+
+class Point:
+    """Represents a point in a 2 dimensional space
+    """
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
+point = Point(12, 15)
+
+print point
